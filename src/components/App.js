c2be26a84bdd52bb4940fcf7e4e76efb0b17a89c
Hassan Masood <iamhassanmasood@gmail.com>
2022-03-11 18:55:15 +0500

    task:arrows and swipe added

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,51 @@ import {
   UpOutlined,
   PlusCircleOutlined,
   MinusCircleOutlined,
+  LeftOutlined,
+  RightOutlined,
 } from "@ant-design/icons";
 
 const { Paragraph } = Typography;
+
+const SamplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <LeftOutlined
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        fontSize: "20px",
+        color: "black",
+      }}
+      onClick={onClick}
+    />
+  );
+};
+
+const SampleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <RightOutlined
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        fontSize: "20px",
+        color: "black",
+      }}
+      onClick={onClick}
+    />
+  );
+};
 
 export default function App(props) {
   const [records, setRecords] = useState([]);
   const [selectedDate, setSelectedDate] = useState("Today");
   const [timeVal, setTimeVal] = useState("00:00");
+  const [durationVal, setDurationVal] = useState("00:00");
+  const [durationHours, setDurationHours] = useState(0);
+  const [durationMinutes, setDurationMinutes] = useState(0);
   const [show, setShow] = useState(true);
   const generateDates = (props) => {
     const date = moment(props.firstDate);
@@ -54,13 +91,39 @@ export default function App(props) {
 
   const carouselSettings = {
     autoplay: true,
+    pauseOnHover: true,
     slidesToShow: 4,
     slidesToScroll: 1,
+    speed: 100,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
 
   function handleTimeChange(time) {
     let _time = moment(time).format("HH:mm");
     setTimeVal(_time);
+  }
+  function handleDurationChange(time) {
+    let _time = moment(time).format("HH:mm");
+    setDurationVal(_time);
+    let arr = _time.split(":");
+    setDurationHours(Number(arr[0]));
+    setDurationMinutes(Number(arr[1]));
+  }
+
+  function onIncreaseTime() {
+    if (durationMinutes < 59 && durationMinutes >= 0) {
+      setDurationMinutes((x) => x + 1);
+    } else if (durationMinutes === 59) {
+      setDurationHours((x) => x + 1);
+      setDurationMinutes(0);
+    }
+  }
+
+  function onDecreaseTime() {
+    if (durationMinutes > 0) {
+      setDurationMinutes((x) => x - 1);
+    }
   }
 
   return (
@@ -91,7 +154,13 @@ export default function App(props) {
           {show ? (
             <Row>
               <Col span={24} className="carousal-div">
-                <Carousel autoplay {...carouselSettings}>
+                <Carousel
+                  autoplay
+                  {...carouselSettings}
+                  swipeToSlide
+                  draggable
+                  arrows
+                >
                   {records.map((item, idx) => (
                     <DateCard
                       month={item.month}
@@ -113,28 +182,52 @@ export default function App(props) {
                   <Paragraph className="date-link">Time</Paragraph>
                   <Row className="date-hide-show">
                     <TimePicker
-                      style={{ border: "none" }}
+                      style={{ width: "60px" }}
+                      defaultValue={moment(timeVal, "HH:mm")}
                       value={moment(timeVal, "HH:mm")}
                       format={"HH:mm"}
                       onChange={(time) => handleTimeChange(time)}
+                      suffixIcon={null}
+                      clearIcon={null}
+                      bordered={false}
                     />
                     <DownOutlined style={{ fontSize: "20px" }} />
                   </Row>
                 </Col>
               </Row>
-              <Row>
-                <Col span={24}>
-                  <Paragraph className="date-link">Duration</Paragraph>
-                  <PlusCircleOutlined
-                    className="date-hide-show"
-                    style={{ fontSize: "24px" }}
-                  />
-                  <MinusCircleOutlined
-                    className="date-hide-show"
-                    style={{ fontSize: "24px" }}
-                  />
-                </Col>
-              </Row>
+
+              {timeVal !== "00:00" ? (
+                <Row>
+                  <Col span={24}>
+                    <Paragraph className="date-link">Duration</Paragraph>
+                    <Row className="date-hide-show">
+                      <PlusCircleOutlined
+                        onClick={onIncreaseTime}
+                        style={{ fontSize: "24px" }}
+                      />
+                      <TimePicker
+                        style={{ width: "60px" }}
+                        defaultValue={moment(durationVal, "HH:mm")}
+                        value={moment(
+                          `${durationHours}:${durationMinutes}`,
+                          "HH:mm"
+                        )}
+                        format={"HH:mm"}
+                        onChange={(time) => handleDurationChange(time)}
+                        suffixIcon={null}
+                        clearIcon={null}
+                        bordered={false}
+                      />
+                      <MinusCircleOutlined
+                        onClick={onDecreaseTime}
+                        style={{ fontSize: "24px" }}
+                      />
+                    </Row>
+                  </Col>
+                </Row>
+              ) : (
+                ""
+              )}
             </>
           )}
         </>
--- src/components/DateCard.js
+++ src/components/DateCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Typography } from "antd";
+import { Card, Typography, Row, Col } from "antd";
 const { Title, Paragraph } = Typography;
 
 import "antd/dist/antd.css";
@@ -13,17 +13,21 @@ export default function DateCard({
   slected,
 }) {
   return (
-    <Card
-      title={month}
-      style={{ width: 170, height: 180, textAlign: "center" }}
-      onClick={!disabled ? () => onChange(slected) : () => {}}
-    >
-      <Title level={1} style={{ color: "gray", fontWeight: "normal" }}>
-        {date}
-      </Title>
-      <Paragraph style={{ color: "gray", fontSize: "20px", bottom: "0" }}>
-        {disabled ? "Closed" : day}
-      </Paragraph>
-    </Card>
+    <Row>
+      <Col span={24}>
+        <Card
+          title={month}
+          style={{ width: 170, height: 180, textAlign: "center" }}
+          onClick={!disabled ? () => onChange(slected) : () => {}}
+        >
+          <Title level={1} style={{ color: "gray", fontWeight: "normal" }}>
+            {date}
+          </Title>
+          <Paragraph style={{ color: "gray", fontSize: "20px", bottom: "0" }}>
+            {disabled ? "Closed" : day}
+          </Paragraph>
+        </Card>
+      </Col>
+    </Row>
   );
 }
