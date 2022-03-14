316375a3ca0765b48e8beceb7953deea5b0e8972
Hassan Masood <iamhassanmasood@gmail.com>
2022-03-14 18:55:48 +0500

    index: code refactorized

--- src/index.js
+++ src/index.js
@@ -3,19 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./styles.less";
 
-ReactDOM.render(
-  <App
-    firstDate={"2022-03-10"}
-    lastDate={"2022-07-10"}
-    disabledDates={[
-      "2022-03-15",
-      "2022-03-18",
-      "2022-03-12",
-      "2022-03-21",
-      "2022-03-26",
-      "2022-03-28",
-    ]}
-    numberOfDays={123}
-  />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
