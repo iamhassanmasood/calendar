41146d4116a80ce7e7bcc33ac9e290fdac85e321
Hassan Masood <iamhassanmasood@gmail.com>
2022-03-17 20:34:55 +0500

    helpers: remove extra function and name changed

--- src/helpers/formats.js
+++ src/helpers/formats.js
@@ -1,15 +1,7 @@
 import moment from "moment";
-export const selectedDateFormat = (date) => {
+export const dateFormater = (date = new Date()) => {
   let format = `${moment(date).format("D")}. ${moment(date).format(
     "MMMM"
   )} ${moment(date).format("YYYY")}`;
   return format;
-};
-
-export const currentDateFormat = () => {
-  let date = new Date();
-  let currentDate = `${moment(date).format("D")}. ${moment(date).format(
-    "MMMM"
-  )} ${moment(date).format("YYYY")}`;
-  return currentDate;
 };
