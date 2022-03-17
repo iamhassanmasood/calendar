39f2bcf665cb6fb54f454cb3eef03b202c3f1512
Hassan Masood <iamhassanmasood@gmail.com>
2022-03-17 20:31:10 +0500

    App: removed extra code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,5 @@ import React, { useState } from "react";
 import Calendar from "./components/Calendar/Calendar";
 
 export default function App() {
-  const [selectedDate, setSelectedDate] = useState("");
-  function helloCalling(date) {
-    setSelectedDate(date);
-  }
-  console.log(selectedDate); //////////)
-
-  return <Calendar onDateChange={helloCalling} />;
+  return <Calendar />;
 }
