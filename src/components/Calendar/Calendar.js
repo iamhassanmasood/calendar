13660aef06b7cca64a5f7c995c5cd5a51a4e480f
Hassan Masood <iamhassanmasood@gmail.com>
2022-03-17 20:32:19 +0500

    Calendar: names updated and arrow function used

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -6,7 +6,7 @@ import DateSelector from "../DateSelector/DateSelector";
 import TimeSelector from "../TimeSelector/TimeSelector";
 import DurationSelector from "../DurationSelector/DurationSelector";
 import CarouselCards from "../CarouselCards/CarouselCards";
-import { currentDateFormat } from "../../helpers/formats";
+import { dateFormater } from "../../helpers/formats";
 
 function Calendar(props) {
   const [records, setRecords] = useState([]);
@@ -15,27 +15,23 @@ function Calendar(props) {
   const [durationValue, setDurationValue] = useState("00:00");
   const [durationHours, setDurationHours] = useState(0);
   const [durationMinutes, setDurationMinutes] = useState(0);
-  const [show, setShow] = useState(true);
+  const [showCalendar, setShowCalendar] = useState(true);
 
-  const generateDates = (props) => {
+  const generateDates = () => {
     const startDate = moment(props.firstDate);
     const disabledDates = props.disabledDates ? props.disabledDates : [];
-
-    const firstDate = props.firstDate
-      ? moment(props.firstDate)
-      : moment(new Date());
     const lastDate = props.lastDate ? moment(props.lastDate) : null;
 
     const numberOfDays = lastDate
-      ? moment.duration(lastDate.diff(firstDate)).asDays() + 1
+      ? moment.duration(lastDate.diff(startDate)).asDays() + 1
       : props.numberOfDays;
 
-    const dates = [];
+    const dateRange = [];
     for (let i = 0; i < numberOfDays; i += 1) {
       const isDisabled = !!disabledDates.includes(
         startDate.format("YYYY-MM-DD")
       );
-      dates.push({
+      dateRange.push({
         date: startDate.format("YYYY-MM-DD"),
         day: startDate.format("D"),
         dayOfWeek: startDate.format("dddd"),
@@ -44,84 +40,78 @@ function Calendar(props) {
       });
       startDate.add(1, "days");
     }
-    return setRecords(dates);
+    return setRecords(dateRange);
   };
 
-  useEffect(() => generateDates(props), []);
+  useEffect(() => generateDates(), []);
 
   useLayoutEffect(() => {
     props.onDateChange(selectedDate);
-    let currentDate = currentDateFormat();
-    if (selectedDate == currentDate) setSelectedDate("Today");
+    let currentDate = dateFormater();
+    selectedDate == currentDate ? setSelectedDate("Today") : "";
   }, [selectedDate]);
 
-  function handleTimeChange(time) {
-    let Time = moment(time).format("HH:mm");
-    setTimeValue(Time);
-  }
-  function handleDurationChange(time) {
-    let Time = moment(time).format("HH:mm");
-    setDurationValue(Time);
-    let arr = Time.split(":");
-    setDurationHours(Number(arr[0]));
-    setDurationMinutes(Number(arr[1]));
-  }
+  const handleTimeChange = (time) => {
+    let formattedTime = time.format("HH:mm");
+    setTimeValue(formattedTime);
+  };
+  const handleDurationChange = (time) => {
+    let formattedTime = time.format("HH:mm");
+    setDurationValue(formattedTime);
+    let durationArray = formattedTime.split(":");
+    setDurationHours(Number(durationArray[0]));
+    setDurationMinutes(Number(durationArray[1]));
+  };
 
-  function onIncreaseDuration() {
+  const onIncreaseDuration = () => {
     if (durationMinutes < 59 && durationMinutes >= 0) {
       setDurationMinutes((x) => x + 1);
     } else if (durationMinutes === 59) {
       setDurationHours((x) => x + 1);
       setDurationMinutes(0);
     }
-  }
+  };
 
-  function onDecreaseDuration() {
+  const onDecreaseDuration = () => {
     if (durationMinutes > 0) {
       setDurationMinutes((x) => x - 1);
     }
-  }
+  };
 
   return (
-    <Row>
-      <Col span={12} offset={6}>
-        <DateSelector
-          show={show}
-          onShow={() => setShow((x) => !x)}
-          selectedDate={selectedDate}
-        />
-        <>
-          {show ? (
-            <CarouselCards
-              {...props}
-              records={records}
-              call={setSelectedDate}
+    <Col span={12} offset={6}>
+      <DateSelector
+        show={showCalendar}
+        onShow={() => setShowCalendar((x) => !x)}
+        selectedDate={selectedDate}
+      />
+      <>
+        {showCalendar ? (
+          <CarouselCards {...props} records={records} call={setSelectedDate} />
+        ) : (
+          <>
+            <TimeSelector
+              value={moment(timeValue, "HH:mm")}
+              defaultValue={moment(timeValue, "HH:mm")}
+              format={"HH:mm"}
+              handleChange={(time) => handleTimeChange(time)}
             />
-          ) : (
-            <>
-              <TimeSelector
-                value={moment(timeValue, "HH:mm")}
-                defaultValue={moment(timeValue, "HH:mm")}
-                format={"HH:mm"}
-                handleChange={(time) => handleTimeChange(time)}
-              />
 
-              {timeValue !== "00:00" ? (
-                <DurationSelector
-                  handleIncrease={onIncreaseDuration}
-                  handleDecrease={onDecreaseDuration}
-                  handleChange={(time) => handleDurationChange(time)}
-                  value={moment(`${durationHours}:${durationMinutes}`, "HH:mm")}
-                  defaultValue={moment(durationValue, "HH:mm")}
-                />
-              ) : (
-                ""
-              )}
-            </>
-          )}
-        </>
-      </Col>
-    </Row>
+            {timeValue !== "00:00" ? (
+              <DurationSelector
+                handleIncrease={onIncreaseDuration}
+                handleDecrease={onDecreaseDuration}
+                handleChange={(time) => handleDurationChange(time)}
+                value={moment(`${durationHours}:${durationMinutes}`, "HH:mm")}
+                defaultValue={moment(durationValue, "HH:mm")}
+              />
+            ) : (
+              ""
+            )}
+          </>
+        )}
+      </>
+    </Col>
   );
 }
 
@@ -150,7 +140,7 @@ Calendar.defaultProps = {
   speed: 100,
   numberOfDays: 30,
   disabledDates: [
-    "2022-03-17",
+    "2022-03-19",
     "2022-03-18",
     "2022-03-21",
     "2022-03-24",
